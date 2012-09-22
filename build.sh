a42f1fac445a5e5b2bf7e985c7e72a9eed1fe2df
Jari Aalto <jari.aalto@cante.net>
2012-09-23 00:33:17 +0300

    import original  CYGWIN-PATCHES 2012-09-22

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Copyright (C) YYYY Firstname Lastname; Licensed under GPL v2 or later
+#
+# build.sh -- Custom build steps
+#
+#   Parameters send to this script
+#
+#       build.sh <PACKAGE> <VERSION> <RELEASE>
+# 
+
+${CC:-gcc} $CPPFLAGS $CFLAGS  $LDFLAGS \
+  -D__GLIBC__ \
+  -o pax \
+  ar.c ar_io.c ar_subs.c buf_subs.c cache.c cpio.c file_subs.c \
+  ftree.c gen_subs.c getoldopt.c options.c pat_rep.c pax.c \
+  sel_subs.c tables.c tar.c tty_subs.c \
+  -lc
+                                                                                                                            
+# End of file
